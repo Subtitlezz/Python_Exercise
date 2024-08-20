5dd0e3dcd2febd98f27aa8d2089c602caf1efde3
Subtitlezz <wmaciver@proton.me>
2024-08-21 05:00:53 +1000

    updated

---
+++ MoreDictonaryThings.py
@@ -0,0 +1,8 @@
+travel_log = {
+    'France': {
+        'cities_visited': ['Paris', 'Lille', 'Dijon'],
+        'Total_visits': 12
+    },
+}
+
+print(travel_log['France']['cities_visited'][1], travel_log['France']['Total_visits'])
