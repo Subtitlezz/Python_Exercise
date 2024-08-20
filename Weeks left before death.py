1f0b974cf12d738f7ef8c3826e491389cff53a38
Subtitlezz <wmaciver@proton.me>
2024-08-20 14:31:26 +1000

    Delete Weeks left before death.py

--- Weeks left before death.py
+++
@@ -1,8 +0,0 @@
-age = input()
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-age_int = int(age)
-max_age = 90
-diff = 90 - age_int
-weeks = 52
-print(f'You have {int(diff * weeks)} weeks left.')
